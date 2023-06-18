ed0a96cc8acd6c08efe006d1c654362b17fcf908
cheekysim <minecraftcheekysim@gmail.com>
2023-06-18 03:17:09 +0100

    Removed wip stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import "./assets/css/App.css";
 import { Hero } from "./components/Hero";
-import { Langcards } from "./components/Langcards";
 
 function App() {
   return (
     <>
       <Hero />
-      <Langcards />
     </>
   );
 }
