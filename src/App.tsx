bbb4e365bae0dad0a18125bf1813ab790caea1ca
cheekysim <minecraftcheekysim@gmail.com>
2023-06-30 16:54:59 +0100

    added toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./assets/css/App.css";
 import { Hero } from "./components/Hero";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
+      <Toaster position="top-right" />
       <Hero />
     </>
   );
